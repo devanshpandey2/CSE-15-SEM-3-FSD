cd5a3b5752254a3885439a3a8af2d84627fa7825
devanshpandey2 <pandeydevanshhhh@gmail.com>
2026-09-01 11:58:26 +0530

    01/09

---
+++ backend/exp2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const fileName = "sample.txt";
+
+//create
+fs.writeFileSync(fileName, "Hello, this is the original content.");
+console.log("File created successfully.");
+
+
+let data = fs.readFileSync(fileName, "utf8");
+console.log("File content:", data);
+
+// UPDATE
+fs.appendFileSync(fileName, "\nThis content has been updated.");
+console.log("File updated successfully.");
+
+data = fs.readFileSync(fileName, "utf8");
+console.log("Updated content:", data);
+
+// DELETE
+fs.unlinkSync(fileName);
+console.log("File deleted successfully.");a
